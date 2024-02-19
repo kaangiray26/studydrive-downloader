35c412ed04707dcea46320422d367ec54a8c6b70
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-02-19 20:23:28 +0100

    build

--- background.js
+++ background.js
@@ -1,22 +1,58 @@
+// background.js
+// This is the background script
+
+var filename = "";
+var requests_data = [];
+
+async function downloadfile() {
+    console.log("Downloading file...");
+    // Check if there are any files to download
+    if (!requests_data.length) {
+        console.log("No files to download");
+        return;
+    }
+
+    // Create a new blob from the requests_data array
+    const blob = new Blob(requests_data, { type: "application/pdf" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename + ".pdf";
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+function header_listener(details) {
+    console.log("Request:", details);
+
+    // Reset the values
+    filename = new URL(details.originUrl).pathname.split("/").slice(-2).join("_");
+    requests_data = [];
+}
+
 function listener(details) {
     let filter = browser.webRequest.filterResponseData(details.requestId);
     filter.ondata = (event) => {
-        console.log("Request:", details.url);
+        console.log("Request URL:", details.url);
+
+        // Add array buffer to the requests_data array
+        requests_data.push(event.data);
 
         // Construct a filename for the requested file
-        const filename = new URL(details.originUrl).pathname.split("/").slice(-2).join("_");
+        // const filename = new URL(details.originUrl).pathname.split("/").slice(-2).join("_");
 
         // Download the file
-        const blob = new Blob([event.data], { type: "application/pdf" });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
+        // const blob = new Blob([event.data], { type: "application/pdf" });
+        // requests_data.push(blob);
+        // const url = URL.createObjectURL(blob);
+        // const a = document.createElement("a");
 
-        // Download without opening a new tab
-        a.href = url;
-        a.download = filename + ".pdf";
-        a.click();
+        // // Download without opening a new tab
+        // a.href = url;
+        // a.download = filename + ".pdf";
+        // a.click();
 
-        URL.revokeObjectURL(url);
+        // URL.revokeObjectURL(url);
         filter.write(event.data);
     };
     filter.onstop = (event) => {
@@ -28,10 +64,21 @@ function listener(details) {
     };
 }
 
+// Get request data
 browser.webRequest.onBeforeRequest.addListener(
     listener,
     { urls: ["https://www.studydrive.net/file-preview/*"] },
-    ["blocking"],
+    ["blocking"]
 );
+
+// Get response headers
+browser.webRequest.onHeadersReceived.addListener(
+    header_listener,
+    { urls: ["https://www.studydrive.net/file-preview/*"] },
+    ["responseHeaders"],
+);
+
+// Button click listener
+browser.browserAction.onClicked.addListener(downloadfile);
 
 console.log(Date.now(), "Background script started...");
